8f6c30a9f5911972f59decf20aa2246c636a7839
natalya <rymnat@mail.ru>
2021-11-11 12:43:43 +0700

    HW

--- src/test/java/lesson3/ImageTest.java
+++ src/test/java/lesson3/ImageTest.java
@@ -1,5 +1,6 @@
 package lesson3;
 
+import io.restassured.mapper.ObjectMapperType;
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -19,6 +20,8 @@ public class ImageTest extends BaseTest {
     private final String PATH_TO_IMAGE = "src/test/resources/carbon.png";
     static String encodedFile;
     String uploadedImageId;
+    String imageHash;
+    String title = "new_title";
 
     @BeforeEach
     void beforeTest(){
@@ -26,17 +29,127 @@ public class ImageTest extends BaseTest {
         encodedFile = Base64.getEncoder().encodeToString(byteArray);
     }
 
-    @AfterEach
-    void clear(){
-        given()
-                .header("Authorization", token)
+
+
+//    @Test
+//    void uploadFileTest() {
+//        uploadedImageId = given()
+//                .headers("Authorization", token)
+//                .multiPart("image", encodedFile)
+//                .expect()
+//                .body("success", is(true))
+//                .body("data.id", is(notNullValue()))
+//                .when()
+//                .post("https://api.imgur.com/3/image")
+//                .prettyPeek()
+//                .then()
+//                .extract()
+//                .response()
+//                .jsonPath()
+//                .getString("data.deletehash");
+//    }
+
+//    @Test
+//    void uploadFileImageTest() {
+//        uploadedImageId = given()
+//                .headers("Authorization", token)
+//                .multiPart("image", new File("src/test/resources/carbon.png"))
+//                .expect()
+//                .statusCode(200)
+//                .when()
+//                .post("https://api.imgur.com/3/upload")
+//                .prettyPeek()
+//                .then()
+//                .extract()
+//                .response()
+//                .jsonPath()
+//                .getString("data.deletehash");
+//
+//    }
+
+    @Test
+    void uploadFileImageTest() {
+        imageHash = given()
+                .headers("Authorization", token)
+                .multiPart("image", new File("src/test/resources/carbon.png"))
+                .expect()
+                .statusCode(200)
                 .when()
-                .delete("https://api.imgur.com/3/image/{{deleteHash}}", "testprogmath", uploadedImageId)
+                .post("https://api.imgur.com/3/upload")
+                .prettyPeek()
+                .then()
+                .extract()
+                .response()
+                .jsonPath()
+                .getString("data.id");
+
+    }
+
+    @Test
+    void getImageTest() {
+        given()
+                .headers("Authorization", token)
+                .when()
+                .get("https://api.imgur.com/3/image/{imageHash}", imageHash)
                 .prettyPeek()
                 .then()
                 .statusCode(200);
 
     }
+
+    @Test
+    void uploadFileWithoutImageTest() {
+        given()
+                .headers("Authorization", token)
+                .when()
+                .post("https://api.imgur.com/3/upload")
+                .prettyPeek()
+                .then()
+                .statusCode(400);
+
+    }
+
+//    @Test
+//    void updateImageInformationTest() {
+//        given()
+//                .headers("Authorization", token)
+////                .multiPart("image", new File("src/test/resources/carbon.png"))
+//                .body(title)
+//                .expect()
+//                .statusCode(200)
+//                .when()
+//                .post("https://api.imgur.com/3/image/{deleteHash}", uploadedImageId)
+//                .prettyPeek()
+//                .then()
+//                .extract()
+//                .response()
+//                .jsonPath()
+//                .getString("data.deletehash");
+//
+//    }
+
+
+//    @AfterEach
+//    void tearDown() {
+//        given()
+//                .headers("Authorization", token)
+//                .when()
+//                .delete("https://api.imgur.com/3/account/{username}/image/{deleteHash}", "testprogmath", uploadedImageId)
+//                .prettyPeek()
+//                .then()
+//                .statusCode(200);
+//    }
+
+//    @AfterEach
+//    void tearDown() {
+//        given()
+//                .headers("Authorization", token)
+//                .when()
+//                .delete("https://api.imgur.com/3/image/{deleteHash}", uploadedImageId)
+//                .prettyPeek()
+//                .then()
+//                .statusCode(200);
+//    }
 
     private byte[] getFileContent(){
         byte [] byteArray = new byte[0];
@@ -46,27 +159,6 @@ public class ImageTest extends BaseTest {
             e.printStackTrace();
         }
         return byteArray;
-    }
-
-    @Test
-    void imageUpload(){
-        uploadedImageId = given()
-                .header("Authorization", token)
-                .multiPart("Image", encodedFile)
-                .expect()
-//                .statusCode(200)
-                .body("data.id", is(notNullValue()))
-//                .body("data.name", equalTo("frame_name"))
-//                .body("data.title", equalTo("frame_title"))
-                .when()
-                .post("https://api.imgur.com/3/image")
-                .prettyPeek()
-                .then()
-                .extract()
-                .response()
-                .jsonPath()
-                .getString("data.deletehash");
-
     }
 
 }
