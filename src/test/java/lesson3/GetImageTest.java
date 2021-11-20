f2986403940d0ff1ca2672bd62d3d7d3d6a96c1d
mardgebass <85385782+mardgebass@users.noreply.github.com>
2021-11-20 21:32:39 +0700

    body.as

--- src/test/java/lesson3/GetImageTest.java
+++ src/test/java/lesson3/GetImageTest.java
@@ -1,5 +1,6 @@
 package lesson3;
 
+import dto.ImageResponce;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -22,9 +23,9 @@ public class GetImageTest extends BaseTest{
                 .prettyPeek()
                 .then()
                 .extract()
-                .response()
-                .jsonPath()
-                .getString("data.id");
+                .body()
+                .as(ImageResponce.class)
+                .getData().getId();
     }
 
     @Test
